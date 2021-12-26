6359de5a3a2ba3ed09f273c34117d24daeb87740
seeker <74554612+Ldtenacity@users.noreply.github.com>
2021-12-26 20:17:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ from model import Cell
 from settings import *
 
 embedding, word2idx = process.load_embedding("models/corpus.word2vec")#加载embeddings
+Q = [] #问题
+T = [] #正确答案
+F = [] #错误选项
 stop_words = codecs.open("models/unused.txt", 'r', encoding='utf8').readlines()
 for i in range(0,len(stop_words)):
     stop_words[i]=stop_words[i].strip()
 process.prepare_data("models/database.txt", "data/train.txt", stop_words)
 train_sim_ixs = process.cal_scores("data/train.txt", stop_words, sim_num)#计算最高相似性的几个知识
 test_sim_ixs = process.cal_scores("data/test.txt", stop_words, sim_num)
+#加载训练集和测试集合
 train_questions, train_answers, train_labels, train_question_num = process.load_data("models/database.txt", "data/train.txt", word2idx, stop_words, train_sim_ixs, max_len)
-
 test_questions, test_answers, test_labels, test_question_num = process.load_data("models/database.txt", "data/test.txt", word2idx, stop_words, test_sim_ixs, max_len)
-
-
-questions, true_answers, false_answers = [], [], []#将数据按照epoch进行划分
 for q, ta, fa in process.pre_epoch(
-        train_questions, train_answers, train_labels, train_question_num, length):questions.append(q), true_answers.append(ta), false_answers.append(fa)
+        train_questions, train_answers, train_labels, train_question_num, length): Q.append(q), T.append(ta), F.append(fa)
 
 def cal_params():
+    #计算参数
     print("evaluating..")
     scores = []
     for test_q, test_a in process.lat_epoch(test_questions, test_answers, test_question_num, length):
@@ -62,6 +63,7 @@ def cal_params():
     print("evaluation acc(train): ", cnt / train_question_num)
 
 def update_params():
+    #更新参数
     sess.run(tf.compat.v1.global_variables_initializer())
     lr = learning_rate
     for i in range(shr_rate):
@@ -69,7 +71,7 @@ def update_params():
         optimizer.apply_gradients(zip(grads, tvars))
         trainOp = optimizer.apply_gradients(zip(grads, tvars), global_step=globalStep)
         for epoch in range(num_epochs):
-            for question, trueAnswer, falseAnswer in zip(questions, true_answers, false_answers):
+            for question, trueAnswer, falseAnswer in zip(Q, T, F):
                 feed_dict = {
                     lstm.Train_Q: question,
                     lstm.Train_A: trueAnswer,
@@ -81,23 +83,27 @@ def update_params():
                 summary_writer.add_summary(summary, step)
                 if step % train_period == 0:
                     cal_params()
+            #每轮训练结尾记录中间结果
             saver.save(sess, "res/savedModel")
         lr *= reg_factor
 
 with tf.Graph().as_default(), tf.device("/gpu:0"):
     gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.7)
+    #创建sess对象最为计算环境
     session_conf = tf.compat.v1.ConfigProto(allow_soft_placement=True,gpu_options=gpu_options)
     with tf.compat.v1.Session(config=session_conf).as_default() as sess:
         globalStep = tf.compat.v1.Variable(0, name="global_step", trainable=False)
         lstm = Cell(length, max_len, embedding, embedding_dim, rnn_size, upper_bound)
         tvars = tf.compat.v1.trainable_variables()
-        grads, _ = tf.compat.v1.clip_by_global_norm(tf.gradients(lstm.loss, tvars), max_grad_norm)
+        #计算梯度下降参数
+        grads, _ = tf.compat.v1.clip_by_global_norm(tf.gradients(lstm.loss, tvars), iter_ub)
         saver = tf.compat.v1.train.Saver()
         # 计算时间
         timestamp = str(int(time.time()))
+        #时间写入runs文件夹
         out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
         print("Writing to {}\n".format(out_dir))
-        #记录参数
+        #记录参数：包括每轮的损失
         loss_summary = tf.compat.v1.summary.scalar("loss", lstm.loss)
         summary_op = tf.compat.v1.summary.merge([loss_summary])
         summary_dir = os.path.join(out_dir, "summary", "train")
